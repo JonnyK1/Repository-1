77bc2fb33fc7f2c25c401adb465fb813d0f370f2
Jonny Klausmeyer <jonnyklausmeyer.yahoo.com>
2022-05-17 15:00:47 -0500

    SecondProjectCommit

--- index.js
+++ index.js
@@ -1,4 +1,27 @@
-setTimeout(wakeUpUser, 5000);
-function wakeUpUser() {
-    alert("New Location!");
-}
+const computerChoiceDisplay = document.getElementById('computer-choice')
+const userChoiceDisplay = document.getElementById('user-choice')
+const resultDisplay = document.getElementById('result')
+const possibleChoices = document.querySelectorAll('button')
+let userChoice
+let computerChoice
+
+possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click', (e) => {
+    userChoice = e.target.id
+    userChoiceDisplay.innerHTML = userChoice
+    generateComputerChoice()
+}))
+ 
+function generateComputerChoice() {
+     const randomNumber = Math.floor(Math.random() * 3) + 1
+     
+     if (randomNumber === 1) {
+        computerChoice = 'Rock'
+     }
+     if (randomNumber === 2) {
+        computerChoice = 'Scissors'
+    }
+    if (randomNumber === 3) {
+        computerChoice = 'Paper'
+    }
+    computerChoiceDisplay.innerHTML = computerChoice
+ }
